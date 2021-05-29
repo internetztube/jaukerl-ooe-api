df46fbe8fe98e6e7e791ba62fcfd0909b90a491a
Frederic Koeberl <frederic.koeberl@dynatrace.com>
2021-05-29 11:05:57 +0200

    Rename appointmentsByAuthorityId to appointmentsByAuthority.

--- service.js
+++ service.js
@@ -5,7 +5,7 @@ const getAuthorities = async () => {
   return await request.json()
 }
 
-const appointmentsByAuthorityId = async (authority) => {
+const appointmentsByAuthority = async (authority) => {
   const request = await fetch(`https://e-gov.ooe.gv.at/at.gv.ooe.cip/services/api/covid/slots?page=1&size=1000&orgUnitId=${authority.orgUnitId}&birthdate=1990-01-01`)
   let appointments = await request.json()
   appointments = appointments.map(o => {
@@ -21,7 +21,7 @@ const main = async () => {
   let appointments = []
   for (let i = 0; i < authorities.length; i++) {
     const authority = authorities[i]
-    const result = await appointmentsByAuthorityId(authority)
+    const result = await appointmentsByAuthority(authority)
     appointments = Object.assign(appointments, result)
   }
   return appointments
